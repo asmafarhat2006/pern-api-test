d35a4675741acebbe10655ba783d66bbf55d7254
Asma Qureshi <asma.farhat2003@gmail.com>
2021-06-30 12:26:20 +0100

    change config

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
   app.use('/api', createProxyMiddleware({
-    target: 'localhost:3001'
+    target: 'http://localhost:3001',
+    changeOrigin:true
  //   target: 'https://pern-api-test.herokuapp.com',
    
   }));
