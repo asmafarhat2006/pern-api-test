7a9f175bd1dafb409e2892849b2fbe7f8598f4ac
Asma Qureshi <asma.farhat2003@gmail.com>
2021-06-30 12:41:13 +0100

    change config

--- server/index.js
+++ server/index.js
@@ -135,15 +135,15 @@ const swaggerDocument = require('./swagger.json');
 
   // Serves Swagger API documentation to /docs url
   app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-/*
+
   if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, '../build')));
   // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+      res.sendFile(path.join(__dirname, '../build', 'index.html'));
     });
-  }*/
+  }
 
   app.listen(port, () => {
   console.log(`App running on port ${port}.`)
